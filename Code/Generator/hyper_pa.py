e50a5eaa2b529594e105ca1fc2cae63365dea06f
daqh <valeriodipasq@gmail.com>
2023-07-26 15:40:52 +0200

    Complete HyperPA

--- Code/Generator/hyper_pa.py
+++ Code/Generator/hyper_pa.py
@@ -1,11 +1,13 @@
 
 import networkx as nx
 import hypernetx as hnx
+from networkx.algorithms import bipartite
 import random
 import math
 import matplotlib.pyplot as plt
+import argparse
 
-def hyper_pa(S: list[int], NP: list[int], n: int):
+def hyper_pa(S: list[int], NP: list[int], n: int, trials: int, alpha: float):
     '''Hypergraph Preferential Attachment
     
     Parameters
@@ -16,35 +18,69 @@ def hyper_pa(S: list[int], NP: list[int], n: int):
         Distribution of number of new hyperedges
     n : int
         Number of nodes
+    trials : int
+        Number of trials
+    alpha : float
+        Awareness parameter
     '''
     G = nx.Graph()
     # Initialize G with s_max/2 disjoint hyperedges of size 2
     s_max = max(S)
     for i in range(s_max//2 + 1):
-        G.add_node(i + 1, bipartite=0)
-        G.add_node(s_max - i, bipartite=0)
-        G.add_node(str(i + 1), bipartite=1)
-        G.add_edges_from([(str(i + 1), s_max - i)])
-        G.add_edges_from([(str(i + 1), i + 1)])
+        G.add_node(i + 1, bipartite=1)
+        G.add_node(s_max - i, bipartite=1)
+        G.add_node(f'c{i + 1}', bipartite=0)
+        G.add_edges_from([(f'c{i + 1}', s_max - i)])
+        G.add_edges_from([(f'c{i + 1}', i + 1)])
 
-    for i in  range(1, n):
+    for i in  range(s_max//2 + 1, n + 1):
+        print(i)
         k = random.choice(NP) # Sample a number k from NP
-        for j  in range(1, k):
+        for j  in range(1, k + 1):
             s = random.choice(S) # Sample a hyperedge size s from S
             if s == 1:
-                G.add_node(i, bipartite=0)
-                G.add_node(str(i), bipartite=1)
-                G.add_edges_from([(i, str(i))]) # Add the hyperedge {i} to G
+                G.add_node(i, bipartite=1)
+                G.add_node(f"c{j}", bipartite=0)
+                G.add_edge(i, f'c{i}') # Add the hyperedge {i} to G
             # else if all (s-1)-sized groups have 0 degree then
+            elif len(G.nodes) < s - 1 or all(G.degree[i] == 0 for i in {n for n, d in G.nodes(data=True) if d["bipartite"] == 1}):
+                # Choose s-1 nodes randomly
+                nodes = random.choices(range(1, n), k=s-1)
+                # Add the hyperedge of i and the s-1 nodes to G
+                G.add_nodes_from(nodes, bipartite=1)
+                G.add_node(i, bipartite=1)
+                G.add_node(f'c{j}', bipartite=0)
+                G.add_edge(i, f'c{j}')
+                G.add_edges_from([(l, f'c{j}') for l in nodes])
+            else:
+                # Choose a group of size s-1 with probability proportional to degree
+                top_nodes = {n for n, d in G.nodes(data=True) if d["bipartite"] == 1}
+                nodes = random.choices(list(top_nodes), weights=[G.degree[i] for i in top_nodes], k=s-1)
+                # Add the hyperedge of i and the s-1 nodes to G
+                G.add_nodes_from(nodes, bipartite=1)
+                G.add_node(i, bipartite=1)
+                G.add_node(f'c{j}', bipartite=0)
+                G.add_edge(i, f'c{j}')
+                G.add_edges_from([(l, f'c{j}') for l in nodes])
     return G
 
-def main():
-    H = hyper_pa([1, 15, 24], [1, 2, 3], 40)
-    print(list(H.edges))
+def main(*args, **kwargs):
+    with open("simplex per node/email-Eu-simplices-per-node-distribution.txt") as f:
+        S = [int(x) for x in f.read().splitlines()]
+    with open("size distribution/email-Eu size distribution.txt") as f:
+        NP = [int(x) for x in f.read().splitlines()]
+    H = hyper_pa(S, NP, 300, 20, 1)
     H = hnx.Hypergraph.from_bipartite(H)
-    # Print nodes in each hyperedge
-    hnx.draw(H)
-    plt.show()
+    with open('output_directory/output.txt', 'w') as f:
+        for e in H.edges:
+            nodes = sorted(H.edges[e])
+            f.write(str(' ').join(map(str, nodes)) + '\n')
+    # Print the nodes in the edge 1
+    # print(H.incidence_dict)
+    # hnx.draw(H)
+    # plt.show()
+    # Print the list of nodes in each hyperedge
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Hypergraph Preferential Attachment')
     main()
